d1487f605d1a0253c5f5b7ca57ba2367f72c2240
adisetiawannn <adisetiawann52@gmail.com>
2025-08-07 20:04:59 +0700

    first commit

---
+++ notebooks/data-cleaning-and-processing/01_import_dataset.py
@@ -0,0 +1,10 @@
+# execute this code to import the dataset
+
+# if you haven't installed the pandas library, you can do so using:
+# !pip install pandas
+
+import pandas as pd
+# Load the dataset
+df = pd.read_csv()
+
+df.head(10) # Display the first 10 rows of the dataset
